999ac85cdd0379845f703e06c40d0ba76764ac82
Doucet, Mathieu <doucetm@ornl.gov>
2017-08-11 13:23:45 -0400

    start adding tests

--- web_reflectivity/fitting/tests.py
+++ web_reflectivity/fitting/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class LocalStoreTestCase(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+        user = User.objects.create_user('john', 'john@test.com', 'johnpassword')
+        user.save()
+
+    def test_animals_can_speak(self):
+        """Write data in the local store"""
+        self.client.login(username='john', password='johnpassword')
+        response = self.client.get('/fit/files/')
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.context['file_list'], '[]')
