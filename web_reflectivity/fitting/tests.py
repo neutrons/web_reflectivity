c522ff31ee518af094510d7c78dc00543d111bd2
Doucet, Mathieu <doucetm@ornl.gov>
2017-08-14 09:06:08 -0400

    adding tests

--- web_reflectivity/fitting/tests.py
+++ web_reflectivity/fitting/tests.py
@@ -8,12 +8,16 @@ class LocalStoreTestCase(TestCase):
         self.client = Client()
         user = User.objects.create_user('john', 'john@test.com', 'johnpassword')
         user.save()
+        self.client.login(username='john', password='johnpassword')
 
     def test_file_list(self):
         """Write data in the local store"""
-        self.client.login(username='john', password='johnpassword')
         response = self.client.get('/fit/files/')
-
         self.assertEqual(response.status_code, 200)
-
         self.assertEqual(response.context['file_list'], '[]')
+
+        # Upload data
+        with open('test/test_data.txt') as fp:
+            response = self.client.post('/fit/files/', {'id_file': 'test_data.txt', 'attachment': fp})
+            print(response.context['file_list'])
+            #self.assertEqual(response.context['file_list'], '[]')
