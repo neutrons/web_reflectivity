5d77d836087f36bce7658dfb8ea97b28a4e00049
Doucet, Mathieu <doucetm@ornl.gov>
2018-10-02 09:01:40 -0400

    Update simple log parser

--- web_reflectivity/fitting/parsing/refl1d.py
+++ web_reflectivity/fitting/parsing/refl1d.py
@@ -61,6 +61,38 @@ def update_with_results(fit_problem, par_name, value, error):
                     layer.save()
     fit_problem.save()
 
+def update_model_from_dict(fit_problem, experiment):
+    """
+        Parse a json representation of the experiment
+    """
+    for layer in experiment['sample']['layers']:
+        for par_name in ['thickness', 'rho', 'irho', 'interface']:
+            if layer[par_name]['fixed'] is False:
+                update_with_results(fit_problem, '%s %s' % (layer['name'], par_name), layer[par_name]['value'], error=0.0)
+
+    if experiment['probe']['intensity']['fixed'] is False:
+        update_with_results(fit_problem, 'intensity', experiment['probe']['intensity']['value'], error=0.0)
+
+    if experiment['probe']['background']['fixed'] is False:
+        update_with_results(fit_problem, 'intensity', experiment['probe']['background']['value'], error=0.0)
+
+def update_model_from_json(content, fit_problem):
+    """
+        Update a model described by a FitProblem object according to the contents
+        of a REFL1D log.
+
+        :param str content: log contents
+        :param FitProblem fit_problem: fit problem object to update
+    """
+    key_start = 'MODEL_JSON_START'
+    key_end = 'MODEL_JSON_END'
+    _index_start = content.find(key_start)
+    _index_end = content.find(key_end)
+    if _index_start >= 0 and _index_end > 0:
+        _json = content[_index_start+len(key_start):_index_end]
+        _expt = json.loads(_json)
+        update_model_from_dict(fit_problem, _expt)
+
 def update_model(content, fit_problem):
     """
         Update a model described by a FitProblem object according to the contents
@@ -68,20 +100,10 @@ def update_model(content, fit_problem):
 
         :param str content: log contents
         :param FitProblem fit_problem: fit problem object to update
-
-        .. note::
-            [chisq=23.426(15), nllf=1850.62]
-
-                          Parameter       mean  median    best [   68% interval] [   95% interval]
-
-             1            intensity  1.084(31)  1.0991  1.1000 [  1.062   1.100] [  1.000   1.100]
-
-             2              air rho 0.91(91)e-3 0.00062 0.00006 [ 0.0001  0.0017] [ 0.0000  0.0031]
-
     """
     start_err_file = False
-    start_par_file = False
     found_errors = False
+    chi2 = None
     for line in content.split('\n'):
         if start_err_file:
             try:
@@ -92,36 +114,23 @@ def update_model(content, fit_problem):
             except:
                 logging.error("Could not parse line %s", line)
 
-        if start_par_file and not found_errors:
-            try:
-                par_name, value = parse_par_file_line(line)
-                if par_name is not None:
-                    update_with_results(fit_problem, par_name, value, error=0.0)
-            except:
-                logging.error("Could not parse line %s", line)
-
         # Find chi^2, which comes just before the list of parameters
         if line.startswith('[chi'):
             try:
                 result = re.search(r'chisq=([\d.]*)', line)
-                chi2 = result.group(1)
+                chi2 = float(result.group(1))
             except:
-                chi2 = "unknown"
+                chi2 = None
 
         if line.startswith('MODEL_PARAMS_START'):
             start_err_file = True
         if line.startswith('MODEL_PARAMS_END'):
             start_err_file = False
-        if line.startswith('MODEL_BEST_VALUES_START'):
-            start_par_file = True
-        if line.startswith('MODEL_BEST_VALUES_END'):
-            start_par_file = False
 
-    try:
-        chi2_value = float(chi2)
-    except:
-        chi2_value = None
-    return chi2_value
+    # If we didn't use the DREAM algorithm, we won't found the errors and need to parse the full json data
+    if not found_errors:
+        update_model_from_json(content, fit_problem)
+    return chi2
 
 def extract_data_from_log(log_content):
     """
@@ -208,19 +217,3 @@ def extract_multi_sld_from_log(log_content):
         elif data_started is True:
             data_content.append(line)
     return data_block_list
-
-def parse_par_file_line(line):
-    """
-        Parse a line from the __model.par file
-
-        :param str line: string to be parsed
-    """
-    result = re.search(r'^(.*) ([\d.-]+)(e?[\d-]*)', line.strip())
-    value_float = None
-    par_name = None
-    if result is not None:
-        par_name = result.group(1).strip()
-        value = "%s%s" % (result.group(2), result.group(3))
-        value_float = float(value)
-        value_float = float("%g" % value_float)
-    return par_name, value_float
